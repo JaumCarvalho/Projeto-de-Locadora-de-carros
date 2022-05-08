4a893f4bbb37610e12459d9d7cfd0a8b64eef3ae
Guilherme Oliveira dos Anjos <anjosgui12@gmail.com>
2022-05-08 16:36:55 -0300

    Simplificando os serviços
    - CRUD via serviço
    - criptografia de senha

---
+++ Locadora.api/src/main/java/com/locadora/project/locadora/api/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.locadora.project.locadora.api.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import com.locadora.project.locadora.api.model.Usuario;
+
+import com.locadora.project.locadora.api.repository.IUsuario;
+
+
+@Service
+public class UsuarioService {
+	private IUsuario repository;
+	private PasswordEncoder passwordEncoder;
+	
+	
+	public UsuarioService (IUsuario repository) {
+		this.repository = repository;
+		this.passwordEncoder=new BCryptPasswordEncoder();
+	}
+	public List<Usuario> listarUsuario(){
+		List<Usuario> lista = repository.findAll();
+		return lista;
+	}
+	public Usuario criarUsuario(Usuario usuario) {
+		String encoder = this.passwordEncoder.encode(usuario.getSenha());
+		usuario.setSenha(encoder);
+		Usuario usuarioNovo = repository.save(usuario);
+		return usuarioNovo;
+	}
+	public Usuario editarUsuario(Usuario usuario) {
+		Usuario usuarioNovo = repository.save(usuario);
+		return usuarioNovo;
+	}
+	public Boolean excluirUsuario(Integer id) {
+		repository.deleteById(id);
+		return true;
+	}
+	
+}

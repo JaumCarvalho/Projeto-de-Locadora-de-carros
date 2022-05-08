ff4110abe93f8ca26b67debcfc79cb724f6e536e
Guilherme Oliveira dos Anjos <anjosgui12@gmail.com>
2022-05-08 15:07:41 -0300

    Notas

--- Locadora.api/src/main/java/com/locadora/project/locadora/api/model/Usuario.java
+++ Locadora.api/src/main/java/com/locadora/project/locadora/api/model/Usuario.java
@@ -6,24 +6,25 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+// NOTAÇÃO DE ENTIDADE
 @Entity
+// DIRECIONANDO A TABELA NO BANCO DE DADOS
 @Table(name="usuario_db")
 
 public class Usuario {
-	
+	//GERANDO ID AUTOMATICAMENTE 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_")
 	private Integer id;
-	
+	//INSERINDO NA COLUNA EMAIL
 	@Column(name="email_", length=100, nullable = true)
 	private String email;
-
+	// INSERINDO NA COLUNA SENHA
 	@Column(name="senha_", columnDefinition = "TEXT", nullable = true)
 	private String senha;
 	
-	
+	// METODOS GETTERS AND SETTERS
 	public String getSenha() {
 		return senha;
 	}

becfccd50101090d9d7ab28de4c844ab558e6be1
Guilherme Oliveira dos Anjos <anjosgui12@gmail.com>
2022-05-08 16:38:03 -0300

    Simplificando usando serviços

--- Locadora.api/src/main/java/com/locadora/project/locadora/api/controller/UserController.java
+++ Locadora.api/src/main/java/com/locadora/project/locadora/api/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.locadora.project.locadora.api.DAO.IUsuario;
+import com.locadora.project.locadora.api.repository.IUsuario;
+import com.locadora.project.locadora.api.service.UsuarioService;
 import com.locadora.project.locadora.api.model.Usuario;
 
 
@@ -25,26 +27,27 @@ import com.locadora.project.locadora.api.model.Usuario;
 @RequestMapping("/usuarios")
 public class UserController {
 	@Autowired
-	private IUsuario dao;
+	private UsuarioService usuarioService;
+	public UserController(UsuarioService usuarioService) {
+		this.usuarioService=usuarioService;
+	}
 	@GetMapping
-	public List <Usuario> listausuarios () {
-		return (List<Usuario>) dao.findAll();
+	public ResponseEntity<List<Usuario>>listausuarios () {
+		return ResponseEntity.status(200).body(usuarioService.listarUsuario());
 	}
 	@PostMapping
-	public Usuario criarUsuario(@RequestBody Usuario usuario) {
-		Usuario usuarioNovo= dao.save(usuario);
-		return usuarioNovo;
+	public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+
+		return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
 	}
 	@PutMapping
-	public Usuario editarUsuario(@RequestBody Usuario usuario) {
-		Usuario usuarioNovo= dao.save(usuario);
-		return usuarioNovo;
+	public ResponseEntity <Usuario> editarUsuario(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
 	}
 	@DeleteMapping("/{id}")
-	public Optional<Usuario> excluirUsuario(@PathVariable Integer id) {
-		Optional<Usuario> usuario = dao.findById(id);
-		dao.deleteById(id);
-		return usuario;
+	public ResponseEntity<?> excluirUsuario(@PathVariable Integer id) {
+		usuarioService.excluirUsuario(id);
+		return ResponseEntity.status(204).build();
 		
 	}
 	

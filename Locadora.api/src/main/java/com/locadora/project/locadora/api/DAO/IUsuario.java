c9293f2728e76e05707f15b6409192998c1dcbb7
Guilherme Oliveira dos Anjos <anjosgui12@gmail.com>
2022-05-08 15:53:32 -0300

    exclui arquivo duplicado

--- Locadora.api/src/main/java/com/locadora/project/locadora/api/DAO/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.locadora.project.locadora.api.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.locadora.project.locadora.api.model.Usuario;
-public interface IUsuario extends CrudRepository <Usuario, Integer>{
-	
-
-}

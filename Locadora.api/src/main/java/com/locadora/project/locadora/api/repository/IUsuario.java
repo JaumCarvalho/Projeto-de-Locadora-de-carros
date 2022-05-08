665c03b5a56d9d50a5a68a6da36dbe8f187a414a
Guilherme Oliveira dos Anjos <anjosgui12@gmail.com>
2022-05-08 16:39:20 -0300

    Usando JPA repository
    simplificando e entrando no padrão de nomeclatura

--- Locadora.api/src/main/java/com/locadora/project/locadora/api/repository/IUsuario.java
+++ Locadora.api/src/main/java/com/locadora/project/locadora/api/repository/IUsuario.java
@@ -1,11 +1,12 @@
 package com.locadora.project.locadora.api.repository;
 
 
- // IMPORTANDO O CRUD REPOSITORIO
-import org.springframework.data.repository.CrudRepository;
+ import org.springframework.data.jpa.repository.JpaRepository;
+// IMPORTANDO O CRUD REPOSITORIO
+
 
 import com.locadora.project.locadora.api.model.Usuario;
-public interface IUsuario extends CrudRepository <Usuario, Integer>{
+public interface IUsuario extends JpaRepository<Usuario, Integer>{
 	
 
 }
